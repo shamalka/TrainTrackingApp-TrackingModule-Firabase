7338cdfaaea960f69049a64e395f35d661a1ceb0
shamalka <shamalkanavod95@gmail.com>
2018-09-05 00:05:31 +0530

    Get Device Location
    Get device location and send it to firebase database.

--- TrainTracking2/app/src/main/java/com/snov/traintracking/activities/TrackingActivity.java
+++ TrainTracking2/app/src/main/java/com/snov/traintracking/activities/TrackingActivity.java
@@ -1,10 +1,19 @@
 package com.snov.traintracking.activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +22,10 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import android.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.snov.traintracking.R;
 import com.snov.traintracking.utilities.Constants;
 
@@ -20,9 +33,19 @@ public class TrackingActivity extends AppCompatActivity {
 
     TextView Latitude;
     TextView Longitude;
+    TextView LocationData;
 
     private Firebase RefLat;
     private Firebase RefLong;
+
+    private Firebase SendRefLatitude;
+    private Firebase SendRefLongitude;
+
+    private Button SendData;
+    private Button ShareLocation;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +69,9 @@ public class TrackingActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
                 //Log.v("item", (String) parent.getItemAtPosition(position));
-                Toast.makeText(TrackingActivity.this, "Item "+ (String) parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                Toast.makeText(TrackingActivity.this, "Item " + (String) parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 // TODO Auto-generated method stub
@@ -71,8 +95,9 @@ public class TrackingActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
                 //Log.v("item", (String) parent.getItemAtPosition(position));
-                Toast.makeText(TrackingActivity.this, "Item "+ (String) parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                Toast.makeText(TrackingActivity.this, "Item " + (String) parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 // TODO Auto-generated method stub
@@ -81,16 +106,104 @@ public class TrackingActivity extends AppCompatActivity {
 
         Firebase.setAndroidContext(this);
 
-        Latitude = (TextView)findViewById(R.id.latitude);
-        Longitude = (TextView)findViewById(R.id.longitude);
+        Latitude = (TextView) findViewById(R.id.latitude);
+        Longitude = (TextView) findViewById(R.id.longitude);
+        LocationData = (TextView) findViewById(R.id.location_data);
+
+        GetFirebaseData(RefLat, Latitude, "Latitude");
+        GetFirebaseData(RefLong, Longitude, "Longitude");
 
 
+        SendData = (Button) findViewById(R.id.send_button);
 
-        GetFirebaseData(RefLat,Latitude,"Latitude");
-        GetFirebaseData(RefLong,Longitude,"Longitude");
+        SendRefLatitude = new Firebase("https://train-tracking-app.firebaseio.com/train/Latitude");
+        SendRefLongitude = new Firebase("https://train-tracking-app.firebaseio.com/train/Longitude");
+//        SendData.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                SendRefLatitude.setValue("This is Latitude");
+//            }
+//        });
 
+        //RequestPermission();
+
+        ShareLocation = (Button) findViewById(R.id.share_location);
+        LocationData = (TextView) findViewById(R.id.location_data);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                LocationData.setText(location.getLatitude() + " " + location.getLongitude());
+                SendRefLatitude.setValue(location.getLatitude());
+                SendRefLongitude.setValue(location.getLongitude());
+                //Toast.makeText(TrackingActivity.this, location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s){
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        configureButton();
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                configureButton();
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+
+                return;
+
+            }
+        }else{
+            configureButton();
+        }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch(requestCode){
+            case 10:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                return;
+        }
+    }
+
+    private void configureButton(){
+        ShareLocation.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    //Toast.makeText(TrackingActivity.this, "Show Location", Toast.LENGTH_SHORT).show();
+                    locationManager.requestLocationUpdates("gps",1000,0,locationListener);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+
+
+
+            }
+        });
+
+    }
+
+//    private void RequestPermission(){
+//        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+//    }
 
     public void GetFirebaseData(Firebase mRef, final TextView textView, String path){
         try {
